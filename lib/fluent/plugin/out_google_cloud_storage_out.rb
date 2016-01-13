f2399e5ee3bf8af34f20088fc75f489e9dc63f07
Hideki Matsuoka <matsuoka_hideki@cyberagent.co.jp>
2016-01-13 22:12:52 +0900

    Add config for make unique Because if send file with same name, GCS Override the file. Select from chunk_id, timestamp, increment

--- lib/fluent/plugin/out_google_cloud_storage_out.rb
+++ lib/fluent/plugin/out_google_cloud_storage_out.rb
@@ -13,8 +13,16 @@ module Fluent
 
     SUPPORTED_COMPRESS = {
       'gz' => :gz,
-      'gzip' => :gzip,
+      'gzip' => :gz,
     }
+
+    UNIQUE_STRATEGY= {
+      'chunk_id' => :chunk_id,
+      'increment' => :increment,
+      'timestamp' => :timestamp,
+    }
+
+    UNIQUE_PLACE_HOLDER = '${unique}'
 
     #
     # Config Parameters
@@ -39,6 +47,18 @@ module Fluent
 
     #desc "The tag for out"
     config_param :default_tag, :string, :default => 'tag_missing'
+
+    #desc "The unique strategy for avoid override for same path"
+    config_param :unique_strategy, :default => nil do |val|
+      c = UNIQUE_STRATEGY[val]
+      unless c
+        raise ConfigError, "Unsupported make uniuqe strategy '#{val}'"
+      end
+      c
+    end
+
+    #desc "The format for unique replacement, if you set timestamp to unique_strategy, you should set time format"
+    config_param :unique_format, :string, :default => "%Y%m%d%H%M%S%L"
 
     #desc "Compress flushed file."
     config_param :compress, :default => nil do |val|
@@ -75,9 +95,18 @@ module Fluent
 
       super
 
+      if @path.index(UNIQUE_PLACE_HOLDER).nil? && @unique_strategy
+        raise Fluent::ConfigError, "Path must contain ${unique}, or you set the unique_strategy to nil."
+      end
+
       @formatter = Plugin.new_formatter(@format)
       @formatter.configure(conf)
+      @path_suffix = ".log"
       prepare_client()
+
+      if @unique_strategy == :increment
+         @samepath_counter = 0
+      end
     end
 
     def prepare_client
@@ -103,15 +132,54 @@ module Fluent
       @formatter.format(tag, time, record)
     end
 
-    def path_format(chunk_key)
-      path = Time.strptime(chunk_key, @time_slice_format).strftime(@path)
-      log.debug "GCS Path: #{path}"
-      path
+    def chunk_unique_id_to_str(unique_id)
+      unique_id.unpack('C*').map{|x| x.to_s(16).rjust(2,'0')}.join('')
+    end
+
+    def path_format_with_unique_strategy(path, strategy, chunk_key, chunk_unique)
+       case strategy
+       when nil
+         path
+       when :chunk_id
+         path.gsub(UNIQUE_PLACE_HOLDER, chunk_unique_id_to_str(chunk_unique))
+       when :increment
+         if @before_chunk_key
+           if @before_chunk_key == chunk_key
+             @samepath_counter += 1
+           else
+             @samepath_counter = 0
+           end
+         end
+         @before_chunk_key = chunk_key
+         path.gsub(UNIQUE_PLACE_HOLDER, "#{@samepath_counter}")
+       when :timestamp
+         path.gsub(UNIQUE_PLACE_HOLDER, Time.now.strftime(@unique_format))
+       end
+    end
+
+    def path_format(chunk)
+      # format from chunk key
+      path = Time.strptime(chunk.key, @time_slice_format).strftime(@path)
+
+      # format for make unique
+      path = path_format_with_unique_strategy(path, @unique_strategy, chunk.key, chunk.unique_id)
+
+      # append .log
+      unless path.include?(".log")
+        path.concat(@path_suffix)
+      end
+
+      # append .gz
+      case @compress
+      when nil
+        path
+      when :gz
+        "#{path}.gz"
+      end
     end
 
     def send(path, data)
       mimetype = MIME::Types.type_for(path).first
-
       io = nil
       if @compress
         io = StringIO.new("")
@@ -127,10 +195,9 @@ module Fluent
     end
 
     def write(chunk)
-      gcs_path = path_format(chunk.key)
+      gcs_path = path_format(chunk)
       send(gcs_path, chunk.read)
       gcs_path
     end
   end
 end
-
